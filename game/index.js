1df356666f7c4371bb8b818a801105f6bca02669
almondmilk96 <kara.hirschman@gmail.com>
2018-07-05 12:06:34 -0400

    wrote reducer function

--- game/index.js
+++ game/index.js
@@ -1,6 +1,26 @@
 import {Map} from 'immutable'
+import { createStore } from 'redux'
+let board = Map()
 
 export default function reducer(state, action) {
-  // TODO
-  return state
+  switch (action.type) {
+    case MOVE: 
+      board = board.setIn(action.position, action.player)
+      if (action.player === 'X') {
+        board = board.set(turn, 'O')
+      } else {
+        board = board.set(turn, 'X')
+      }
+    default: 
+      return state
+  }
+}
+
+
+function MOVE(player, position) {
+  return {
+    type: MOVE,
+    position: position,
+    player: player
+  }
 }
