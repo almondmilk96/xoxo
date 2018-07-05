54bd4be3d2d312eaa7d31f81c90ee28222fd6d32
Alexandra Ash <alexandraleah@gmail.com>
2018-07-05 13:05:10 -0400

    working on winning function

--- game/index.js
+++ game/index.js
@@ -1,47 +1,48 @@
-import {Map} from 'immutable'
-import { createStore } from 'redux'
-let board = Map()
-const initialState = {turn: 'X', board: board}
+import { Map } from 'immutable';
+import { createStore } from 'redux';
+let board = Map();
+const initialState = { turn: 'X', board: board };
 
-const MOVE = 'MOVE'
+const MOVE = 'MOVE';
 
 export function move(player, position) {
   return {
     type: MOVE,
     position: position,
-    player: player
-  }
+    player: player,
+  };
 }
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
     case MOVE:
-      const newState = Object.assign({}, state)
-      const newBoard = state.board.setIn(action.position, action.player)
-      newState.board = newBoard
+      const newState = Object.assign({}, state);
+      const newBoard = state.board.setIn(action.position, action.player);
+      newState.board = newBoard;
       if (newState.turn === 'X') {
-        newState.turn = 'O'
+        newState.turn = 'O';
       } else {
-        newState.turn = 'X'
+        newState.turn = 'X';
       }
       return newState;
     default:
-      return state
+      return state;
   }
 }
 
-function winner(board){
+function winner(board) {
   //draw
   // X wins
   // 0 wins
   // game is ongoing
-  let winner = 'X';
+  let winner = null;
+  if (streak(board)) {
+    return;
+  }
   return winner;
 }
 
 //spaces are in an array
-function streak(board, spaces ){
-  spaces.forEach(space =>)
+function streak(board, spaces) {
+  //streak returns X, 0, NULL
 }
-
-
